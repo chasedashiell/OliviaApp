d843289df47f0daa543b6f5cebca6a2ebe34854c
chasedashiell <dashchax@gmail.com>
2023-04-04 21:40:17 -0500

    Added removeDuplicates

--- CSVReader.java
+++ CSVReader.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class CSVReader {
-    public static ArrayList<String[]> CSVToArrayListOfArrays(String path) {
+    public static ArrayList<String[]> CSVToArrayListOfStringArrays(String path) {
         String line = "";
         ArrayList<String[]> solution = new ArrayList<>();
         try {
@@ -27,9 +27,29 @@ public class CSVReader {
         return solution;
     }
 
+    public static ArrayList<String[]> removeDuplicates(ArrayList<String[]> list){
+        String[] currArray;
+        String[] toCheck;
+        for (int i = 0; i < list.size(); i++){
+            currArray = list.get(i);
+            for (int j = i+1; j < list.size(); j++){
+                toCheck = list.get(j);
+                if (currArray[0].equals(toCheck[0]) && currArray[1].equals(toCheck[1]) && currArray[2].equals(toCheck[2])){
+                    System.out.println(i);
+                    list.remove(i);
+                }
+            }
+        }
+        return list;
+    }
+
     public static void main(String[] args) {
         String path = "C:\\Users\\dashc\\Downloads\\Copy of Reading List - Sheet1.csv";
-        ArrayList<String[]> one = CSVToArrayListOfArrays(path);
-        System.out.println(one);
+        ArrayList<String[]> one = CSVToArrayListOfStringArrays(path);
+        one = removeDuplicates(one);
+        for(int i = 0; i < one.size(); i++){
+            System.out.println(Arrays.toString(one.get(i)));
+        }
+        System.out.println(Arrays.toString(one.get(65)));
     }
 }
